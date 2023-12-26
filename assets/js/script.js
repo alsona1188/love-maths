d0bc7abc590ab103b26d6d55f9c4f05c80147ebc
alsona1188 <>
2023-12-26 19:00:22 +0000

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
+function runGame (){
 
+}
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+// create the function to display our questions
+
+function displayAdditionQuestion(){
+
+}
+function displaySubtractQuestion() {
+
+}
+function displayMultiplyQuestion() {
+
+}
